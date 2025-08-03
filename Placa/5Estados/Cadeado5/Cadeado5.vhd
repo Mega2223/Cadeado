b0940d240e86860100ebc7d7fdd11eb7207936d8
Mega2223 <59067466+Mega2223@users.noreply.github.com>
2025-08-03 17:42:20 -0300

    Isso compila

---
+++ Placa/5Estados/Cadeado5/Cadeado5.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Cadeado5 is
+	port(
+		SW: in std_logic_vector(0 downto 0);
+		Z: out std_logic
+   );
+end Cadeado5;
+
+architecture behavior of Cadeado5 is
+begin
+
+end;
